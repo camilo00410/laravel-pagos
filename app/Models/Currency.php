234ad372dbb0cb07bef6bd1c3e921c3ad16376e6
Camilo loaiza <camiloandres00410@gmail.com>
2022-03-04 20:14:17 -0500

    creacion del proyecto, de los modelos y tablas esenciales para la plataforma de pagos

---
+++ app/Models/Currency.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    protected $primaryKey = 'iso';
+    protected $incrementing = false;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'iso',
+    ];
+}
