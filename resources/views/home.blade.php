8786f04f6c517afe7067e37aae24e4d552015f17
Camilo loaiza <camiloandres00410@gmail.com>
2022-03-04 21:43:32 -0500

    Mostrando las plataformas de pago disponibles con laravel y bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,6 +37,31 @@
                             </div>
                         </div>
 
+                        <div class="row mt-3">
+                            <div class="col">
+                                <label>Select the desired payment platform:</label>
+                                <div class="form-group">
+                                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                        @foreach ($paymentPlatforms as $paymentPlatform)
+                                            <label
+                                                class="btn btn-outline-secondary rounded m-2 p-1"
+                                                data-target="#{{ $paymentPlatform->name }}Collapse"
+                                                data-toggle="collapse"
+                                            >
+                                                <input
+                                                    type="radio"
+                                                    name="payment_platform"
+                                                    value="{{ $paymentPlatform->id }}"
+                                                    required
+                                                >
+                                                <img class="img-thumbnail" src="{{ asset($paymentPlatform->image) }}">
+                                            </label>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="text-center mt-3">
                             <button type="submit" id="payButton" class="btn btn-primary btn-lg">Pay</button>
                         </div>
